b33bad0275d0b1e5e35fea5b0da75d312db6b52f
Raul Fernandes <raulmff@gmail.com>
2025-11-22 01:24:14 -0300

    fix env

--- app.config.js
+++ app.config.js
@@ -1,39 +1,39 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_DEV = () => process.env.APP_VARIANT?.trim() === 'development';
+const IS_PREVIEW = () => process.env.APP_VARIANT?.trim() === 'preview';
 
 const getUniqueIdentifier = () => {
-  if (IS_DEV) {
-    return 'com.yourname.stickersmash.dev';
-  }
+	if (IS_DEV()) {
+		return 'com.yourname.stickersmash.dev';
+	}
 
-  if (IS_PREVIEW) {
-    return 'com.yourname.stickersmash.preview';
-  }
+	if (IS_PREVIEW()) {
+		return 'com.yourname.stickersmash.preview';
+	}
 
-  return 'com.yourname.stickersmash';
+	return 'com.yourname.stickersmash';
 };
 
 const getAppName = () => {
-  if (IS_DEV) {
-    return 'StickerSmash (Dev)';
-  }
+	if (IS_DEV()) {
+		return 'StickerSmash (Dev)';
+	}
 
-  if (IS_PREVIEW) {
-    return 'StickerSmash (Preview)';
-  }
+	if (IS_PREVIEW()) {
+		return 'StickerSmash (Preview)';
+	}
 
-  return 'StickerSmash: Emoji Stickers';
+	return 'StickerSmash: Emoji Stickers';
 };
 
 export default ({ config }) => ({
-    ...config,
-    name: getAppName(),
-    ios: {
-        ...config.ios,
-        bundleIdentifier: getUniqueIdentifier(),
-    },
-    android: {
-        ...config.android,
-        package: getUniqueIdentifier(),
-    },
+	...config,
+	name: getAppName(),
+	ios: {
+		...config.ios,
+		bundleIdentifier: getUniqueIdentifier(),
+	},
+	android: {
+		...config.android,
+		package: getUniqueIdentifier(),
+	}
 });
